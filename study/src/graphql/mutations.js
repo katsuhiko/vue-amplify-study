09a82ccf09a6d8880b9c42866e826dffc3aaffa3
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2019-06-27 20:33:46 +0900

    feat: DynamoDB のソートキーとGSIを指定するように変えました。

--- study/src/graphql/mutations.js
+++ study/src/graphql/mutations.js
@@ -1,27 +1,27 @@
 // eslint-disable
 // this is an auto generated file. This will be overwritten
 
-export const createTodo = `mutation CreateTodo($input: CreateTodoInput!) {
-  createTodo(input: $input) {
+export const createStudyAttr = `mutation CreateStudyAttr($input: CreateStudyAttrInput!) {
+  createStudyAttr(input: $input) {
     id
-    name
-    description
+    attr_type
+    attr_value
   }
 }
 `;
-export const updateTodo = `mutation UpdateTodo($input: UpdateTodoInput!) {
-  updateTodo(input: $input) {
+export const updateStudyAttr = `mutation UpdateStudyAttr($input: UpdateStudyAttrInput!) {
+  updateStudyAttr(input: $input) {
     id
-    name
-    description
+    attr_type
+    attr_value
   }
 }
 `;
-export const deleteTodo = `mutation DeleteTodo($input: DeleteTodoInput!) {
-  deleteTodo(input: $input) {
+export const deleteStudyAttr = `mutation DeleteStudyAttr($input: DeleteStudyAttrInput!) {
+  deleteStudyAttr(input: $input) {
     id
-    name
-    description
+    attr_type
+    attr_value
   }
 }
 `;
--- study/src/graphql/queries.js
+++ study/src/graphql/queries.js
@@ -1,24 +1,55 @@
 // eslint-disable
 // this is an auto generated file. This will be overwritten
 
-export const getTodo = `query GetTodo($id: ID!) {
-  getTodo(id: $id) {
+export const getStudyAttr = `query GetStudyAttr($id: ID!, $attr_type: String!) {
+  getStudyAttr(id: $id, attr_type: $attr_type) {
     id
-    name
-    description
+    attr_type
+    attr_value
   }
 }
 `;
-export const listTodos = `query ListTodos(
-  $filter: ModelTodoFilterInput
+export const listStudyAttrs = `query ListStudyAttrs(
+  $id: ID
+  $attr_type: ModelStringKeyConditionInput
+  $filter: ModelStudyAttrFilterInput
   $limit: Int
   $nextToken: String
 ) {
-  listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+  listStudyAttrs(
+    id: $id
+    attr_type: $attr_type
+    filter: $filter
+    limit: $limit
+    nextToken: $nextToken
+  ) {
     items {
       id
-      name
-      description
+      attr_type
+      attr_value
+    }
+    nextToken
+  }
+}
+`;
+export const listStudyAttrsByType = `query ListStudyAttrsByType(
+  $attr_type: String
+  $attr_value: ModelStringKeyConditionInput
+  $filter: ModelStudyAttrFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listStudyAttrsByType(
+    attr_type: $attr_type
+    attr_value: $attr_value
+    filter: $filter
+    limit: $limit
+    nextToken: $nextToken
+  ) {
+    items {
+      id
+      attr_type
+      attr_value
     }
     nextToken
   }

1931745d264b5d3feda7cacc7aefe4b0f8f147a2
Katsuhiko Nagashima <katsuhiko.nagashima@gmail.com>
2019-06-29 10:26:41 +0900

    feat: ログインに関する処理を修正しました。

---
+++ study/src/components/pages/LoginPage.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data () {
+    return {
+      authConfig: {
+        signInConfig: {
+          isSignUpDisplayed : false
+        }
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout justify-center align-center>
+        <amplify-authenticator :authConfig="authConfig"></amplify-authenticator>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
--- study/src/router.js
+++ study/src/router.js
@@ -3,35 +3,37 @@ import Router from 'vue-router'
 
 import HomePage from './components/pages/HomePage.vue'
 import AboutPage from './components/pages/AboutPage.vue'
+import LoginPage from './components/pages/LoginPage.vue'
 import EmployeeAddPage from './components/pages/EmployeeAddPage.vue'
 
-import { components, AmplifyEventBus, AmplifyPlugin } from 'aws-amplify-vue'
-import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyEventBus, AmplifyPlugin } from 'aws-amplify-vue'
+import * as AmplifyModules from 'aws-amplify'
 
 Vue.use(Router)
 Vue.use(AmplifyPlugin, AmplifyModules)
 
 let user
+
 getUser().then((user, error) => {
   if (!user) {
     router.push({ path: '/login' })
   }
 })
 
-function getUser() {
+function getUser () {
   return Vue.prototype.$Amplify.Auth.currentAuthenticatedUser().then((data) => {
     if (data && data.signInUserSession) {
       return data
     }
   }).catch((e) => {
     return null
-  });
+  })
 }
 
 AmplifyEventBus.$on('authState', async (state) => {
   if (state === 'signedOut') {
     user = null
-    router.push({ path: '/auth' })
+    router.push({ path: '/login' })
   } else if (state === 'signedIn') {
     user = await getUser()
     router.push({ path: '/login' })
@@ -59,22 +61,22 @@ const router = new Router({
     },
     {
       path: '/login',
-      name: 'Authenticator',
-      component: components.Authenticator
+      name: 'LoginPage',
+      component: LoginPage
     }
   ]
 })
 
 router.beforeResolve(async (to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
-    user = await getUser();
+    user = await getUser()
     if (!user) {
       return next({
-        path: '/auth',
+        path: '/login',
         query: {
-          redirect: to.fullPath,
+          redirect: to.fullPath
         }
-      });
+      })
     }
     return next()
   }
